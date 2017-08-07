a20ede3dfa8741b0bca5341cb08895fa31930a25
alexwanyoike <wanyoike.alexander@gmail.com>
2017-08-07 11:38:41 +0100

    Initial implementation of plugin

---
+++ Plugin/AttributeSetCodeProductRepositoryPlugin.php
@@ -0,0 +1,32 @@
+<?php
+
+
+use Magento\Catalog\Api\Data\ProductInterface;
+
+class AttributeSetCodeProductRepositoryPlugin
+{
+    private $attributeSetCodeRepository;
+
+    public function __construct(\SnowIO\AttributeSetCode\Model\AttributeSetCodeRepository $attributeSetCodeRepository)
+    {
+        $this->attributeSetCodeRepository = $attributeSetCodeRepository;
+    }
+
+    public function beforeSave(
+        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
+        ProductInterface $product,
+        $saveOptions = false
+    ) {
+        if (!$extensionAttributes = $product->getExtensionAttributes()) {
+            return [$product, $saveOptions];
+        }
+
+        if (null === ($attributeSetCode = $extensionAttributes->getAttributeSetCode())) {
+            return [$product, $saveOptions];
+        }
+        $attributeSetId = $this->attributeSetCodeRepository->getAttributeSetId(4, $attributeSetCode);
+        $product->setAttributeSetId($attributeSetId);
+
+        return [$product, $saveOptions];
+    }
+}
