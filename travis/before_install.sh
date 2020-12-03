aa0d717628126f6449b56202759423bcecb0ef38
Liam Toohey <lt@amp.co>
2020-12-03 15:18:40 +0000

    Disable Xdebug in travis

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 composer self-update 1.10.16
+phpenv config-rm xdebug.ini || true
 cd ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d
 mv xdebug.ini xdebug.ini.bak
 echo 'memory_limit = -1' >> travis.ini
